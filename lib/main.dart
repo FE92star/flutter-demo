ffc26bd484dfd0dc60832643975bf05244224388
MbMin <1737765939@qq.com>
2019-06-18 16:51:56 +0800

    update message

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
+        title: new Text('Startup Name Generators'),
         actions: <Widget>[
           new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
         ],
